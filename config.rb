6ae881c53f8110571d70b9cb2e40870a3a5228ae
Niklas Stephenson <ns@firmafon.dk>
2014-05-18 19:40:04 +0200

    Cleanup

--- config.rb
+++ config.rb
@@ -45,54 +45,7 @@ activate :blog do |blog|
   blog.sources = "{year}-{month}-{day}-{title}.html"
 end
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# Assets
 
 set :css_dir, 'stylesheets'
 
@@ -102,6 +55,7 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 default_caching_policy max_age:(60 * 60 * 24 * 365)
+
 configure :build do
   default_caching_policy max_age:(60 * 60 * 24 * 365)
   
@@ -122,7 +76,4 @@ configure :build do
 
   # Minify HTMl
   activate :minify_html
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
